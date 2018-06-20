7353abdc4a07074e70508e62fc5b0efb4129573f
gitvsks <gmavsks@gmail.com>
2018-06-20 18:21:57 +0900

    1

---
+++ src/Practice.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Practice {
+
+	public static void main(String[] args) {
+		List list = new LinkedList();
+		
+		Iterator it = list.iterator();
+		it.hasNext();
+		it.next();
+		Map map = new HashMap();
+		Set est = map.entrySet();
+	}
+}
---
+++ src/chapter11/IteratorEx1.java
@@ -0,0 +1,34 @@
+package chapter11;
+import java.util.*;
+public class IteratorEx1 {
+
+	public static void main(String[] args) {
+		ArrayList list = new ArrayList();
+		list.add("1");
+		list.add("2");
+		list.add("3");
+		list.add("4");
+		list.add("5");
+		
+		Iterator it = list.iterator();
+		
+		while(it.hasNext()) {
+			Object obj = it.next();
+			System.out.println(obj);
+		}
+		
+		Map map = new HashMap();
+		map.put("1","a");
+		map.put("2","b");
+		map.put("3","c");
+		map.put("4","d");
+		map.put("5","e");
+		
+		Iterator itmap = map.entrySet().iterator();
+		
+		while(itmap.hasNext()) {
+			Object obj = itmap.next();
+			System.out.println(obj);
+		}
+	}
+}
