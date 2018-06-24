6da31ff721b28b3aa8f47c6d4ee34b3cb51dc902
sh <sh@DESKTOP-IR99KSO>
2018-06-24 15:08:58 +0900

    1

--- src/guipractice/ContentPaneEx.java
+++ src/guipractice/ContentPaneEx.java
@@ -8,16 +8,22 @@ public class ContentPaneEx extends JFrame{
 	ContentPaneEx(){
 		setTitle("CotentPane과 JFrame");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
 		Container contentPane = getContentPane();
 		contentPane.setBackground(Color.orange);
+		
+		
 		contentPane.setLayout(new FlowLayout());
+		contentPane.add(new JButton("OK"));
+		contentPane.add(new JButton("Cancel"));
+		contentPane.add(new JButton("Ignore"));
 		contentPane.add(new JButton("OK"));
 		contentPane.add(new JButton("Cancel"));
 		contentPane.add(new JButton("Ignore"));
 		
 		setSize(350,150);
 		setVisible(true);
+		JPanel p = new JPanel();
+		p.setLayout(new FlowLayout());
 	}
 	
 	public static void main(String[] args) {
