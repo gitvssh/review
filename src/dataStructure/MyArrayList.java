f77baabe7a3d9a68db5d25d2a08cd5c6ccc2d43b
SH <SH@58.228.120.235>
2018-07-01 12:49:27 +0900

    practice headfile issue

---
+++ src/dataStructure/MyArrayList.java
@@ -0,0 +1,19 @@
+package dataStructure;
+
+public abstract class MyArrayList {
+
+//	final int LIST_LEN = 100;
+//	int LData;
+//	
+//	MyArrayList(){
+//		int LData[LIST_LEN];
+//		int numOfData;
+//		int curPosition;
+//	}
+//	
+//	MyArrayList plist;
+//	
+//	abstract void MyListInit(MyArrayList plist);
+//	abstract void MyLInsert(MyArrayList plist, LData);
+	
+}
