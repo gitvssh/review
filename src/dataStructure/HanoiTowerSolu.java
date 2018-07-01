b0c81d1b9781f5b12ba866107035d0339345f429
SH <SH@58.228.120.235>
2018-07-01 11:11:16 +0900

    dataStructure

---
+++ src/dataStructure/HanoiTowerSolu.java
@@ -0,0 +1,20 @@
+package dataStructure;
+
+public class HanoiTowerSolu {
+	
+	static void HanoiTowerMove(int num, char from, char by, char to) {
+		if(num==1) {
+			System.out.printf("원반1을 %c에서 %c로 이동  %n",from,to);
+		}else {
+			HanoiTowerMove(num-1,from,to,by);
+			System.out.printf("원반%d을(를) %c에서 %c로 이동 %n",num,from,to);
+			HanoiTowerMove(num-1,by,from,to);
+		}
+	}
+	
+	public static void main(String[] args) {
+		HanoiTowerMove(10,'A','B','C');
+		
+	}
+
+}
---
+++ src/dataStructure/RecursiveBinarySearch.java
@@ -0,0 +1,44 @@
+package dataStructure;
+
+public class RecursiveBinarySearch {
+	static int BsearchRecur(int ar[],int first, int last, int target) {
+		int mid;
+		if(first>last) {
+			return -1;
+		}
+		mid = (first+last)/2;
+		
+		if(ar[mid]==target) {
+			return mid;
+		}else if(target < ar[mid]) {
+			return BsearchRecur(ar, first, mid-1, target);
+		}else {
+			
+			return BsearchRecur(ar, mid+1, last, target);
+		}
+			
+	}
+	
+	public static void main(String[] args) {
+		int arr[] = {1,3,5,7,9};
+		int idx;
+		
+		idx = BsearchRecur(arr, 0, arr.length, 7);
+		if(idx==-1) {
+			System.out.println("탐색실패");
+		}else {
+			System.out.println("타겟 저장 인덱스: "+idx);
+		}
+		
+		idx = BsearchRecur(arr, 0, arr.length, 4);
+		if(idx==-1) {
+			System.out.println("탐색실패");
+		}else {
+			System.out.println("타겟 저장 인덱스: "+idx);
+		}
+		
+		
+			
+	}
+
+}
