b5e157075a6ff4b544a92898a55434629cd4f15d
SH <SH@DESKTOP-FDODE4H>
2018-07-03 06:37:49 +0900

    1

---
+++ src/dataStructure/C4DLinkedList.h
@@ -0,0 +1,38 @@
+#pragma once
+#ifndef __D_LINKED_LIST_H__
+#define __D_LINKED_LIST_H__
+
+#define TRUE 1
+#define FALSE 0
+
+
+
+typedef struct _node
+{
+	LData data;
+	struct _Node *next;
+}Node;
+
+typedef struct _linkdedList
+{
+	Node *head;
+	Node *cur;
+	Node *before;
+	int numOfData;
+	int(*comp)(Ldata d1, Ldata d2);
+}LinkedList;
+
+typedef LinkedList List;
+
+void ListInit(List *plist);
+void LInsert(List *plist, LData data);
+
+int LFirst(List *plist, Ldata *pdata);
+int LNext(List *plist, LData *pdata);
+
+LData LRemove(List *plist);
+int LCount(List *plist);
+
+void SetSortRule(List *plist, int(*comp)(LData d1, LData d2));
+
+#endif
