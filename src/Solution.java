d40270b696da1db3ba6979968c3a8bbaef81d21d
gitvsks <gmavsks@gmail.com>
2018-07-05 17:32:25 +0900

    1

--- src/Solution.java
+++ src/Solution.java
@@ -1,41 +1,45 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
-
-public class Solution {
-    public static void main(String args[] ) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        
-        sc.nextLine();
-        sc.useDelimiter(" ");
-        int n = Integer.parseInt(sc.next());
-        int anna = Integer.parseInt(sc.next());
-        
-        int[] meal = new int[n];
-        sc.nextLine();
-        int i=0;
-        while(sc.hasNext()){
-            meal[i] = Integer.parseInt(sc.next());
-        }
-        //end Input
-        
-        int sum = 0;
-        int actual = 0;
-        int charged = 0;
-        for(int j=0;j<n;j++){
-            if(j==anna){
-                continue;
+class Solution{
+	
+	public static void main(String []argh)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNext()) {
+			String input=sc.next();
+            Stack s = new Stack();//괄호 체크 사용할 스택
+            //Complete the code
+            int len = input.length();//문자열 길이
+            char t = ' ';
+            for(int i=0;i<len;i++){//문자열길이만큼 작업
+                t = input.charAt(i);//char 추출
+                if(t!=')'&&t!='}'&&t!=']'){//닫는괄호인가 확인
+                s.push(input.charAt(i));//아닐경우 스택에 저장
+                    }
+                else{//닫는괄호일경우
+                    if(t==')'&&s.isEmpty()!=true){//괄호가 )일경우, (와 체크
+                        if((char)s.pop()=='(')
+                        continue;
+                    }else if(t=='}'&&s.isEmpty()!=true){//괄호가 }일경우 {와 체크
+                        if((char)s.pop()=='{')
+                        continue;
+                    }else if(t==']'&&s.isEmpty()!=true){//괄호가 ]일경우 [와 체크
+                        if((char)s.pop()=='[')
+                        continue;
+                    }else{//괄호가 일치하지 않는경우
+                        System.out.println("false");
+                        break;
+                    }
+                }
+               if(s.isEmpty()) {
+                   System.out.println("true");
+                   break;
+               }else{
+                   System.out.println("false");
+                    break;
+               }
             }
-            sum += meal[j];
-        }
-        actual = sum/2;
-        charged = (sum+meal[anna])/2;
-        if(actual==charged){
-            System.out.println("Bon Appetit");
-        }else{
-            System.out.println(charged-actual);
-        }
-    }
+		}
+		
+	}
 }
