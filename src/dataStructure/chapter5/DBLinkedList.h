ba4f82c19a45926b30c59cbb33812bbeb315f4ed
SH <SH@DESKTOP-FDODE4H>
2018-07-12 06:28:10 +0900

    success compile

---
+++ src/dataStructure/chapter5/DBLinkedList.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#ifndef __DB_LINKED_LIST_H__
+#define __DB_LINKED_LIST_H__
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int Data;
+
+typedef struct _node
+{
+	Data data;
+	struct _node * next;
+	struct _node * prev;
+}Node;
+
+typedef struct _DLinkedList
+{
+	Node * head;
+	Node * cur;
+	int numOfData;
+}DBLinkedList;
+
+typedef DBLinkedList List;
+
+void ListInit(List * plist);
+void LInsert(List * plist, Data date);
+
+int LFirst(List * plist, Data * data);
+int LNext(List * plist, Data * data);
+int LPrevious(List * plist, Data * pdata);
+int LCount(List * plist);
+
+#endif
