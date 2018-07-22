966b45080bc737fb81572a88218003a3ad833a4f
gitvsks <gmavsks@gmail.com>
2018-07-22 20:52:09 +0900

    queue
    make folder

--- src/dataStructure/chpater6/calculator/InfixCalculator.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#ifndef __INFIX_CALCULATOR__
-#define __INFIX_CALCULATOR__
-
-int EvalInfixExp(char exp[]);
-
-#endif
--- src/dataStructure/chpater6/calculator/PostCalculator.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#ifndef __POST_CALCULATOR_H__
-#define __POST_CALCULATOR_H__
-
-int EvalRPNExp(char exp[]);
-
-#endif
