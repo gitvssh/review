da40113fc066781ab4638ec1ee9ca3bafe7fef51
gitvsks <gmavsks@gmail.com>
2018-08-06 23:33:03 +0900

    dataStructure/chapter13/TableHashFunction.cpp

---
+++ src/c++/ConsoleApplication1/ConsoleApplication1/SimpleHashMain.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+
+int MyHashFunc(int k) {
+	return k % 100;
+}
+
+int main(void) {
+	Table myTbl;
+	Person * np;
+	Person * sp;
+	Person * rp;
+
+	TBLInit(&myTbl, MyHashFunc);
+
+	np = MakePersonData(20120003, "Lee", "Seoul");
+	TBLInsert(&myTbl, GetSSN(np), np);
+
+	np = MakePersonData(20120003, "KIM", "Jeju");
+	TBLInsert(&myTbl, GetSSN(np), np);
+
+	np = MakePersonData(20120003, "HAN", "Kangwon");
+	TBLInsert(&myTbl, GetSSN(np), np);
+
+	sp = TBLSearch(&myTbl, 20120003);
+	if (sp != NULL)
+		ShowPerInfo(sp);
+
+	sp = TBLSearch(&myTbl, 20130012);
+	if (sp != NULL)
+		ShowPerInfo(sp);
+
+	sp = TBLSearch(&myTbl, 20170049);
+	if (sp != NULL)
+		ShowPerInfo(sp);
+
+	rp = TBLDelete(&myTbl, 20120003);
+	if (rp != NULL)
+		free(rp);
+
+	rp = TBLDelete(&myTbl, 20130012);
+	if (rp != NULL)
+		free(rp);
+
+	rp = TBLDelete(&myTbl, 20170049);
+	if (rp != NULL)
+		free(rp);
+
+
+	return 0;
+	
+}
