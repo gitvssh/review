28c97ebf25883ac87206ab5eaffae5e041a4fa20
gitvsks <gmavsks@gmail.com>
2018-08-08 18:21:14 +0900

    python
    exercise

---
+++ src/python/chapter2/ex01.py
@@ -0,0 +1,24 @@
+a=123
+a=-178
+a=0
+a=1.2
+a=3.45
+a=4.24E10
+a=4.24e-10
+a=0o177
+a=0x8ff
+
+a=3
+b=4
+a**b
+
+print("="*50)
+print("My Program")
+print("="*50)
+
+a="Life is too short, You need Python"
+print(a[3])
+print(a[4])
+
+print(a[0:4])
+print(a[12:17])
