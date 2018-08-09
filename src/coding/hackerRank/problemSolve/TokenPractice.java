7e15669bcc7eec46d544759ffd5f6ea61681f8e4
gitvsks <gmavsks@gmail.com>
2018-08-09 15:18:05 +0900

    update package
    2

---
+++ src/coding/hackerRank/problemSolve/TokenPractice.java
@@ -0,0 +1,22 @@
+package coding.hackerRank.problemSolve;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class TokenPractice {
+
+    public static void main(String[] args) {
+    	String s = "Good luck with this case!!! h jrkjc c l m e i vct h ss pqk_v i olf tuoej_          p r jrpjpuo a.   ";
+        StringTokenizer a = new StringTokenizer(s," !,?_'@");
+        
+        int countTokens = a.countTokens();
+        System.out.println(countTokens);
+        for(int i=0;i<countTokens;i++){
+            System.out.println(a.nextToken());
+        }
+    
+        
+        
+    }
+
+}
