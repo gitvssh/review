ce0858bf8b6bc3f76fee6afa276ba8a5529688a5
gitvsks <gmavsks@gmail.com>
2018-08-12 13:09:20 +0900

    dataStructure end
    ~8/12

---
+++ src/dataStructure/chapter14/DFS/ALGraphDFS.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef __AL_GRAPH__
+#define __AL_GRAPH__
+
+#include "stdafx.h"
+
+enum {A,B,C,D,E,F,G,H,I,J};
+
+typedef struct _ual {
+	int numV;
+	int numE;
+	List* adjList;
+	int * visitInfo;
+}ALGraph;
+
+void GraphInit(ALGraph * pg, int nv);
+
+void GraphDestroy(ALGraph * pg);
+
+void AddEdge(ALGraph * pg, int fromV, int toV);
+
+void ShowGraphEdgeInfo(ALGraph * pg);
+
+void DFShowGraphVertex(ALGraph * pg, int starV);
+#endif
