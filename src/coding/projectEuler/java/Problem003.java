eaac5f7f265ccff2c616b0d25f84e8e6a8aab229
gitvsks <gmavsks@gmail.com>
2018-08-22 00:05:31 +0900

    comment

---
+++ src/coding/projectEuler/java/Problem003.java
@@ -0,0 +1,24 @@
+package coding.projectEuler.java;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Problem003 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		The prime factors of 13195 are 5, 7, 13 and 29.
+//
+//		What is the largest prime factor of the number 600851475143 ?
+		double j = 600851475143L;
+		
+		for(double i=1;i<j;i++) {
+			if(j%i==0) {
+				System.out.println(i);
+			}
+			
+		}
+		
+	}
+// 소수구하기, int, double연산, 성능이슈->개선방법, 
+}
