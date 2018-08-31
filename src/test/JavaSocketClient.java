77534af15ec01a4a46cb966183ae227108a027de
gitvssh <gmavsks@gmail.com>
2018-08-31 11:13:39 +0900

    socket exercise
    test for android

---
+++ src/test/JavaSocketClient.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class JavaSocketClient {
+
+	public static void main(String[] args) {
+		try {
+			int portNumber = 5001;
+			Socket sock = new Socket("localhost",portNumber);
+			
+			ObjectOutputStream outstream = new ObjectOutputStream(sock.getOutputStream());
+			outstream.writeObject("Hello Android Town");
+			outstream.flush();
+			
+			ObjectInputStream instream = new ObjectInputStream(sock.getInputStream());
+			System.out.println(instream.readObject());
+			sock.close();
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+}
