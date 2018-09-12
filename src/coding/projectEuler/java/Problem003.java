6e5071cf131b1e5332f2869f9d24aa6a0f8c58d8
gitvssh <gmavsks@gmail.com>
2018-09-12 15:06:16 +0900

    eulerproject 4 solve
    dataStructure, linkedlist, parsing

--- src/coding/projectEuler/java/Problem003.java
+++ src/coding/projectEuler/java/Problem003.java
@@ -7,6 +7,6 @@ public class Problem003 {
 //
 //		What is the largest prime factor of the number 600851475143 ?
 
-		// 소수구하기, int, double연산, 성능이슈->개선방법, 
-// 성능이슈 double 연산속도 너무큼->줄인다,->소인수로 나눠서 수를 작게 만듬, 10분넘게 걸리던 연산이 2ms로 나옴
 }
+// 소수구하기, int, double연산, 성능이슈->개선방법, 
+// 성능이슈 double 연산속도 너무큼->줄인다,->소인수로 나눠서 수를 작게 만듬, 10분넘게 걸리던 연산이 2ms로 나옴
