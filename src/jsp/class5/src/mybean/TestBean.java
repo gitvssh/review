a7d81392fa56e674fca0f14e5bd1adbec95dca72
gitvssh <gmavsks@gmail.com>
2018-09-28 10:45:53 +0900

    moved pacakage annotationing, resume reference

--- src/jsp/class5/src/mybean/TestBean.java
+++ src/jsp/class5/src/mybean/TestBean.java
@@ -1,27 +1,27 @@
-package mybean;
-
-public class TestBean {
-	private String id;//필드, 프로퍼티=property=전역변수
-	private String pwd;
-	private String name;
-	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getPwd() {
-		return pwd;
-	}
-	public void setPwd(String pwd) {
-		this.pwd = pwd;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-}//class
+//package mybean;
+//
+//public class TestBean {
+//	private String id;//필드, 프로퍼티=property=전역변수
+//	private String pwd;
+//	private String name;
+//	
+//	public String getId() {
+//		return id;
+//	}
+//	public void setId(String id) {
+//		this.id = id;
+//	}
+//	public String getPwd() {
+//		return pwd;
+//	}
+//	public void setPwd(String pwd) {
+//		this.pwd = pwd;
+//	}
+//	public String getName() {
+//		return name;
+//	}
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//	
+//}//class
